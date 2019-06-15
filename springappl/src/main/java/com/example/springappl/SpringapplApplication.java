15990cd7e7f044ff5437aeceec12fa469ae96805
Shahana <shaikhshahana67@gmail.com>
2019-06-15 11:29:52 +0530

    Committed

---
+++ springappl/src/main/java/com/example/springappl/SpringapplApplication.java
@@ -0,0 +1,13 @@
+package com.example.springappl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringapplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringapplApplication.class, args);
+	}
+
+}
